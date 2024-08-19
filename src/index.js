1c0d1beb676a4465784e7872f1e71b5cf655b3fa
yogeshpol20 <129049001+yogeshpol20@users.noreply.github.com>
2024-08-20 00:57:28 +0530

    testing no-1

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { NetworkStatusProvider } from "./Components/NetworkStatus";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <NetworkStatusProvider>
-      <App />
-    </NetworkStatusProvider>
-  </BrowserRouter>
-);
-
-
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
 //   <BrowserRouter>
-//     <App />
+//     <NetworkStatusProvider>
+//       <App />
+//     </NetworkStatusProvider>
 //   </BrowserRouter>
 // );
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
