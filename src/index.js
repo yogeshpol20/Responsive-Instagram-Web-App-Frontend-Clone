bd9dd2c47e429607d470e136df70a066f5d8768d
yogeshpol20 <129049001+yogeshpol20@users.noreply.github.com>
2024-08-20 01:47:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { NetworkStatusProvider } from "./Components/NetworkStatus";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <NetworkStatusProvider>
+      <App />
+    </NetworkStatusProvider>
+  </BrowserRouter>
+);
+
+
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
 //   <BrowserRouter>
-//     <NetworkStatusProvider>
-//       <App />
-//     </NetworkStatusProvider>
+//     <App />
 //   </BrowserRouter>
 // );
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
