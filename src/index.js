ce37f9caaba87d3d78e69f1eb098988cd9101609
yogeshpol20 <129049001+yogeshpol20@users.noreply.github.com>
2024-08-20 11:20:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { NetworkStatusProvider } from "./Components/NetworkStatus";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <BrowserRouter>
+  // <HashRouter>
     <NetworkStatusProvider>
       <App />
     </NetworkStatusProvider>
-  // </BrowserRouter>
+  // </HashRouter>
 );
 
 
